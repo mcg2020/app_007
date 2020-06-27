8dd02b6bc861b168dac8b5251a86a228d89c3f39
username <username@email.com>
2020-06-27 17:53:02 +0800

    111

---
+++ scripts/test_setup.py
@@ -0,0 +1,19 @@
+import allure
+
+
+class TestSetup:
+    @allure.severity(allure.severity_level.BLOCKER)
+    @allure.step("我是测试步骤名称")
+    def test_001(self):
+        """添加测试步骤"""
+        allure.attach("我是步骤描述的内容", "附件名字")
+
+    @allure.severity(allure.severity_level.CRITICAL)
+    def test_002(self):
+        assert False
+    @allure.severity(allure.severity_level.NORMAL)
+    def test_003(self):
+        assert True
+    @allure.severity(allure.severity_level.MINOR)
+    def test_004(self):
+        assert True
